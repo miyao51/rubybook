5d1fb5fa798a06cf5098c2591cb224d0874bb7ce
miyao51 <koichi.miyao@gmail.com>
2020-03-03 18:04:20 +0900

    done e10

--- e10.rb
+++ e10.rb
@@ -1,37 +1,34 @@
 require "sinatra"
+
 =begin
-require "net/http"
-require "uri"
-require "json"
-uri = URI.parse("https://igarashikuniaki.net/example.json")
-
-result = Net::HTTP.get(uri)
-hash = JSON.parse(result)
-
-p hash
-p hash["caffe latte"]
-
-=end
 
 # 問1
 get "/omikuji" do 
     ["大吉", "中吉", "末吉", "凶"].sample
 end
 
+
 # 問2
 require "net/http"
 require "uri"
-uri = URI.parse("https://localhost/hi")
 
-http = Net::HTTP.new(uri, "4567")
-#http.use_ssl = false
-p http.get(uri.request_uri)
+uri = URI.parse("http://127.0.0.1/hi")
+req = Net::HTTP::Get.new(uri.path)
+res = Net::HTTP.start(uri.host, '4567') { |http|
+    http.request(req)
+}
+puts res.body
+=end
 
 # 問3
 
-=begin
-uri = URI.parse("https://example.com/some/path")
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-@data = http.get(uri.request_uri)
-=end
+require "net/http"
+require "uri"
+
+uri = URI.parse("http://127.0.0.1/drink")
+req = Net::HTTP::Get.new(uri.path)
+res = Net::HTTP.start(uri.host, '4567') { |http|
+    http.request(req)
+}
+puts res.body
+
--- e10_drink.rb
+++ e10_drink.rb
@@ -3,9 +3,7 @@ get "/hi" do
     "hi!"
 end
 
-=begin
 get "/drink" do
     ["カフェラテ", "モカ", "コーヒー"]
 end
-=end
 
